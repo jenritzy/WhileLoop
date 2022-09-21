4b615700a70623e8fba67b4badc9006c9b655a2c
jenritzy <Jenritzy@outlook.com>
2022-09-21 12:57:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "WhileLoop";
+            int[] nums = new int[10];
+            int i = 0;
+
+            while(i<nums.Length)
+            {
+                nums[i] = i;
+                Console.Write("|" + nums[i]);
+                i++;
+            }
+            Console.Write("\n\n");
+
+            do
+            {
+                i--;
+                if (i == 8) { Console.Write("| Skipped"); continue;}
+                if (i == 3) { Console.Write("| Done"); break; }
+                Console.Write("|" + nums[i]);
+            }
+            while (i > 0);
+            Console.ReadKey();
+        }
+    }
+}
